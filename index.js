3fda2b8f799ce4e2b6b395f4a093cbbe727c4877
Clayton Carlson <claytonrc97@gmail.com>
2023-02-03 00:36:50 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity) {
+    let plan;
+    if (activity === undefined) {
+        plan = "roller-skate";
+    } else {
+        plan = activity;
+    }
+    return "This Saturday, I want to " + plan + "!";
+    }
+
+
+    function mondayWork(job) {
+        let work;
+        if (job === undefined) {
+            job = "go to the office";
+        } else { work = job;
+        } return "This Monday, I will " + job + ".";
+    }
+
+
+    function wrapAdjective(flair = '*') {
+        return function(adjective = "special") {
+          return `You are ${flair}${adjective}${flair}!`;
+        }
+      }
+      
